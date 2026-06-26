2735a0ac9e6234672ceac3e1b2f075831bd015de
megig <39722720+megig@users.noreply.github.com>
2026-06-26 15:37:12 +0700

    Update PMS3003 setup and showValue calls

--- test.ts
+++ test.ts
@@ -2,11 +2,11 @@
 // ใส่โค้ดนี้ใน MakeCode เพื่อทดสอบ
 
 // === on start ===
-PMS3003.setup(SerialPin.P0, SerialPin.P1)
+PMS3003.setup(PMS3003Pin.P0, PMS3003Pin.P1)
 
 // === forever ===
 basic.forever(function () {
     PMS3003.readSensor()
-    PMS3003.showValue(PMS3003.PMType.PM2_5)
+    PMS3003.showValue(PMType.PM2_5)
     basic.pause(2000)
 })
